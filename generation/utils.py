818be10db40241c7fddf03c491fc588e7ac47e75
Mike Kestemont <mike.kestemont@gmail.com>
2017-06-07 15:11:54 +0200

    loader

--- generation/utils.py
+++ generation/utils.py
@@ -3,16 +3,48 @@
 import random
 import glob
 
+import pandas as pd
+
 random.seed(12101985)
+
+
+def load_metadata(fn):
+    df = pd.read_csv(fn, sep=';')
+    df = df.set_index('fname')
+    return df
+
 
 def load_data(path='data/bigmama/',
               include_paragraphs=True,
               paragraph='<par>',
               level='token',
-              max_files=None):
-    
-    filenames = glob.glob(path+'/*.txt')
+              max_files=None,
+              filters={}):
+    """Yields data for training a language model.
+
+    Iterator that yields sentences, tokens or characters for training.
+
+    E.g.
+    filters={'authors':['Pieter Aspe', 'Baantjer'],
+             'titles':['Onder valse vlag']}
+    """
+
+    meta = load_metadata(fn='../meta.csv')
+
+    if filters:
+        filenames = []
+        for fn in glob.glob(path+'/*.txt'):
+            me = meta.loc(os.path.basename(fn))
+            if filters['authors'] and me['author'] not in filters['authors']:
+                continue
+            if filters['titles'] and me['title'] not in filters['titles']:
+                continue
+            filenames.append(fn)
+    else:
+        filenames = glob.glob(path+'/*.txt')
+
     filenames.shuffle()
+
     if max_files:
         filenames = filenames[:max_files]
 
