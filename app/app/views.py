14f9ac9f8471ef6c10645c2883201a9c896dcec7
Folgert Karsdorp <fbkarsdorp@gmail.com>
2017-06-08 15:00:31 +0200

    dummy generation in editor.

--- app/app/views.py
+++ app/app/views.py
@@ -10,17 +10,19 @@ def index():
 def savechange():
     data = flask.request.json
     print(data)
+    # TODO: actually save the changes
     return flask.jsonify(status='OK', message='changes saved.')
 
 @app.route('/savedoc', methods=['POST'])
 def savedoc():
     data = flask.request.json
     print(data)
+    # TODO: actually save the document
     return flask.jsonify(status='OK', message='document saved.')
 
-
+@app.route('/generate', methods=['POST'])
 def generate():
-    return 'generated text'
+    return flask.jsonify(status='OK', message='generated text')
 
 def temperature():
     pass
