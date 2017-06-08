752c6320c0cf2a21f0cde356cc11400ef5aaf918
Folgert Karsdorp <fbkarsdorp@gmail.com>
2017-06-08 14:18:07 +0200

    implement partial saving

--- app/app/views.py
+++ app/app/views.py
@@ -6,6 +6,19 @@ from app import app
 def index():
     return flask.render_template('index.html')
 
+@app.route('/savechange', methods=['POST'])
+def savechange():
+    data = flask.request.json
+    print(data)
+    return flask.jsonify(status='OK', message='changes saved.')
+
+@app.route('/savedoc', methods=['POST'])
+def savedoc():
+    data = flask.request.json
+    print(data)
+    return flask.jsonify(status='OK', message='document saved.')
+
+
 def generate():
     return 'generated text'
 
