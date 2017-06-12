b9121ffa61ebd88e6b8374e2c0739d7204907d4d
Folgert Karsdorp <fbkarsdorp@gmail.com>
2017-06-12 18:14:42 +0200

    add model

--- app/config.py
+++ app/config.py
@@ -3,4 +3,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # model
 MODEL_DIR = os.path.join(basedir, 'models')
-MODEL_NAMES = ['char.LSTM.1l.2546h.46e.100b.2.98.pt']
+MODEL_NAMES = ['char.LSTM.1l.2546h.46e.100b.2.98.pt', 'char.LSTM.1l.2546h.46e.200b.2.49.pt.cpu.pt']
