3c91230980b5239043f0675fc89e6d049e913076
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-06-13 13:57:25 +0200

    removed MODELS_NAME from config.py (not needed)

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -9,6 +9,5 @@ from Synthesizer import Synthesizer
 app = flask.Flask(__name__)
 app.config.from_object('config')
 app.synthesizer = Synthesizer(model_dir=app.config['MODEL_DIR'])
-app.synthesizer.load(model_names=app.config['MODEL_NAMES'])
 
 from app import views
