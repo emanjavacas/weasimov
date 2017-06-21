50f70bfb7ab3ef323a1c3b62baab63d85dbdbe63
Folgert Karsdorp <fbkarsdorp@gmail.com>
2017-06-21 20:48:01 +0200

    rewrite for better sentence segmentation

--- preprocess/senttok.py
+++ preprocess/senttok.py
@@ -1,20 +1,37 @@
 import os
 import ucto
+import lxml.etree
+import pdb
 
-tokenizer = ucto.Tokenizer("tokconfig-nld", quotedetection=True)
+BEGINQUOTE, ENDQUOTE = ucto.TokenRole.BEGINQUOTE, ucto.TokenRole.ENDQUOTE
 
-def split_sentences(string):
-    tokenizer.process(string)
-    sentence = ''
+def split_sentences(text, tokenizer):
+    tokenizer.process(text)
+    sentence, i, open_quote, new_par = '', 0, 0, False
+    prev_ending = False
     for token in tokenizer:
+        i += 1
+        if token.isnewparagraph():
+            new_par = True
+        if prev_ending and token.isbeginofsentence() and not open_quote:
+            yield sentence, new_par
+            sentence, i, new_par = '', 0, False
+        prev_ending = True if token.isendofsentence() else False
+        if token.role == BEGINQUOTE:
+            open_quote += 1
+        elif token == ENDQUOTE:
+            open_quote -= 1
         sentence += str(token)
-        if token.isendofsentence():
-            yield sentence
-            sentence = ''
-        elif not token.nospace():
-            sentence += " "
+        if not token.nospace():
+            sentence += ' '
+        if token.isendofsentence() and i > 200 and open_quote:
+            print("Rollback quote runaway...")
+            print(sentence)
+            tok_backup = ucto.Tokenizer("tokconfig-nld")
+            yield from split_sentences(sentence, tok_backup)
+            sentence, i, open_quote, new_par = '', 0, 0, False
     if sentence:
-        yield sentence
+        yield sentence, new_par
 
 if __name__ == '__main__':
     import argparse
@@ -24,10 +41,15 @@ if __name__ == '__main__':
     parser.add_argument("--extension", type=str, default=".txt")
     args = parser.parse_args()
 
-    for fname in os.listdir(args.input_dir):
+    tokenizer = ucto.Tokenizer("tokconfig-nld", quotedetection=True)
+
+    for i, fname in enumerate(os.listdir(args.input_dir)):
+        print(i, fname)
         if fname.endswith(args.extension):
             with open(os.path.join(args.input_dir, fname)) as infile:
                 text = infile.read()
-            with open(os.path.join(args.output_dir, fname), 'w') as outfile:
-                for sentence in split_sentences(text):
+            with open(os.path.join(args.output_dir, fname), "w") as outfile:
+                for sentence, new_par in split_sentences(text, tokenizer):
+                    if new_par:
+                        outfile.write('\n')
                     outfile.write(sentence + "\n")
