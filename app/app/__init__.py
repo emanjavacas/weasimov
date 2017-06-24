261be93c0c8913d405767c3bde9b8ade9210e889
Folgert Karsdorp <fbkarsdorp@gmail.com>
2017-06-24 13:31:33 +0100

    start database code

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -4,6 +4,7 @@ import sys; sys.path.append(os.path.abspath("../generation"))
 from Synthesizer import Synthesizer
 
 import flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__,
                   static_folder='../static',
@@ -11,6 +12,11 @@ app = flask.Flask(__name__,
 
 # Configure
 app.config.from_object('config')
+
+# Database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "=\x07BoZ\xeb\xb0\x13\x88\xf8mW(\x93}\xe6k\r\xebA\xbf\xff\xb1v"
+db = SQLAlchemy(app)
 
 # Services
 app.synthesizer = Synthesizer(model_dir=app.config['MODEL_DIR'])
