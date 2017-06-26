8faa2a848ad3b6fe7236ad72791c823cf6324cc2
Folgert Karsdorp <fbkarsdorp@gmail.com>
2017-06-26 08:35:18 +0200

    update models

--- app/app/models.py
+++ app/app/models.py
@@ -1,4 +1,6 @@
+import datetime
 from app import db
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,21 +9,25 @@ class User(db.Model):
     surname = db.Column(db.String(64), unique=False)
     password = db.Column(db.String(64), unique=False)
 
+
 class Text(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String())
-    timestamp = db.Column(db.Date)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
 
 class Edit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    startpos = db.Column(db.Integer)
-    endpos = db.Column(db.Integer)
+    start, end = db.Column(db.Integer), db.Column(db.Integer)
     edit = db.String(db.String())
-    timestamp = db.Column(db.Date)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
 
 class Generation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(64))
     seed = db.Column(db.String())
+    temp = db.Column(db.Float)
     text = db.Column(db.String())
-    timestamp = db.Column(db.Date)
-    
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
