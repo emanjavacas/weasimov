e4a3757042688d076e6f2c2bdc87d83b3a553953
Folgert Karsdorp <fbkarsdorp@gmail.com>
2017-06-26 16:37:32 +0200

    add user authentication

--- app/app/models.py
+++ app/app/models.py
@@ -5,9 +5,22 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
-    firstname = db.Column(db.String(64), unique=False)
-    surname = db.Column(db.String(64), unique=False)
     password = db.Column(db.String(64), unique=False)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
+    def __repr__(self):
+        return '<User(%r)>' % self.username
 
 
 class Text(db.Model):
