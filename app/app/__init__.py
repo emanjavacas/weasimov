ae4fef9a97187c60bf5404a2c163740248a0d40f
Folgert Karsdorp <fbkarsdorp@gmail.com>
2017-06-26 16:37:18 +0200

    add required imports

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -5,6 +5,7 @@ from Synthesizer import Synthesizer
 
 import flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = flask.Flask(__name__,
                   static_folder='../static',
@@ -18,7 +19,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "=\x07BoZ\xeb\xb0\x13\x88\xf8mW(\x93}\xe6k\r\xebA\xbf\xff\xb1v"
 db = SQLAlchemy(app)
 
+# login manager
+lm = LoginManager()
+lm.session_protection = 'strong'
+lm.init_app(app)
+lm.login_view = 'login'
+
 # Services
 app.synthesizer = Synthesizer(model_dir=app.config['MODEL_DIR'])
 
-from app import views
+from app import views, models
