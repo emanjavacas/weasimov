12fb60c8315b126491297284514e7a5ecfc3667e
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-06-27 22:46:42 +0200

    added customizable performance impr for lev computation

--- app/static/components/EditorUtils.js
+++ app/static/components/EditorUtils.js
@@ -38,6 +38,8 @@ function findHyps(contentBlock, callback, contentState) {
 
 function insertGeneratedText(editorState, text, data) {
   data['contiguous'] = false;	// HYP entities are not contiguous
+  data['skips'] = 0;
+  data['prevText'] = null;
   const selection = editorState.getSelection();
   const currentContent = editorState.getCurrentContent().createEntity('HYP', 'MUTABLE', data);
   const entityKey = currentContent.getLastCreatedEntityKey();
@@ -70,7 +72,9 @@ function getNormLev(s1, s2) {
 }
 
 
-function updateHypMetadata(editorState) {
+function updateHypMetadata(editorState, totalSkips, triggerOnStringDiff) {
+  totalSkips = totalSkips || 5;
+  triggerOnStringDiff = triggerOnStringDiff || 5;
   const selection = editorState.getSelection();
   if (selection.isCollapsed()) {
     const startKey = selection.getStartKey();
@@ -83,11 +87,22 @@ function updateHypMetadata(editorState) {
       const isCaretOutsideEntity = (entityKeyBeforeCaret !== entityKeyAfterCaret);
       if (entityKeyBeforeCaret && !isCaretOutsideEntity) {
 	const entity = currentContent.getEntity(entityKeyBeforeCaret);
+	const {skips, source, prevText} = entity.getData();
 	if (entity.type === 'HYP') {
 	  const entityText = getEntityText(block, startOffset, entityKeyBeforeCaret);
-	  const sourceText = entity.getData().source;
-	  currentContent.mergeEntityData(
-	    entityKeyBeforeCaret, {'lev': getNormLev(entityText, sourceText)});
+	  const prevTextLength = prevText ? prevText.length : entityText.length;
+	  const diff = Math.abs(prevTextLength - entityText.length);
+	  // only compute lev after 5 edits or an edit of length 5
+	  if (skips >= totalSkips || diff > triggerOnStringDiff) {
+	    currentContent.mergeEntityData(
+	      entityKeyBeforeCaret,
+	      {'lev': getNormLev(entityText, source),
+	       'skips': 0,
+	       'prevText': entityText});
+	  } else {		// increase counters and previous text
+	    currentContent.mergeEntityData(
+	      entityKeyBeforeCaret, {'skips': skips + 1, 'prevText': entityText});
+	  }
 	}
       }
     }
