f02ed6f3da99dcd1f8e4df779ca22deb38775203
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-06-28 09:52:54 +0200

    moved to fast-diff-json

--- app/static/components/app.js
+++ app/static/components/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {EditorState, RichUtils, convertToRaw} from 'draft-js';
 import * as RB from 'react-bootstrap';
 import Sticky from 'react-stickynode';
-import diff from 'deep-diff';
+import jsonpatch from 'fast-json-patch';
 
 import Navbar from './Navbar';
 import ButtonToolbar from './ButtonToolbar';
@@ -111,7 +111,7 @@ class App extends React.Component {
       const currentBlock = EditorUtils.getSelectedBlocks(newContent, selection);
       if (currentBlock.size == 1) {
         const oldBlock = EditorUtils.getSelectedBlocks(oldContent, selection);
-        var d = diff(oldBlock.toJS(), currentBlock.toJS());
+        var d = jsonpatch.compare(oldBlock.toJS(), currentBlock.toJS());
         console.log(JSON.stringify(d));
       }
     }
