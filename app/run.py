8767aa70d3f772277ba7872c3f736b6e0f8c86e7
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-06-30 17:48:52 +0200

    added debug option

--- app/run.py
+++ app/run.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=5000)
     parser.add_argument('--host', default='127.0.0.0')
+    parser.add_argument('--prod', action='store_true')
     args = parser.parse_args()
 
-    app.run(debug=True, port=args.port, host=args.host)
+    app.run(debug=not args.prod, port=args.port, host=args.host)
