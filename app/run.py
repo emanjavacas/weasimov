6a02401ba137f08f7dd336484595898b74a2a1ee
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-06-30 16:41:28 +0200

    added options to run.py

--- app/run.py
+++ app/run.py
@@ -1,4 +1,10 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=5000)
+    parser.add_argument('--host', default='127.0.0.0')
+    args = parser.parse_args()
+
+    app.run(debug=True, port=args.port, host=args.host)
