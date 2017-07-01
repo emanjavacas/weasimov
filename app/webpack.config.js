ab86facbba38196e0e4c9375b4bf8f49df942b32
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-07-01 13:10:08 +0200

    undo webpack config files (will do better in future)

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,5 +1,35 @@
 var webpack = require('webpack');
 
-module.exports = function(env) {
-    return require(`./webpack.${env}.js`);
+module.exports = {  
+  entry: [
+    "./static/components/app.js"
+  ],
+  output: {
+    path: __dirname + '/static',
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+    plugins: [
+	new webpack.DefinePlugin({
+	    'process.env':{
+		'NODE_ENV': JSON.stringify('production')
+	    }
+	}),
+	new webpack.optimize.UglifyJsPlugin({
+	    compress:{
+		warnings: true
+	    }
+	})
+    ]
 };
