d7bfdb9d0139edbc75b0a77366067eedd38cd2fa
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-07-05 21:35:04 +0200

    better webpack config

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -20,16 +20,5 @@ module.exports = {
       }
     ]
   },
-    plugins: [
-	// new webpack.DefinePlugin({
-	//     'process.env':{
-	// 	'NODE_ENV': JSON.stringify('production')
-	//     }
-	// }),
-	// new webpack.optimize.UglifyJsPlugin({
-	//     compress:{
-	// 	warnings: true
-	//     }
-	// })
-    ]
+  plugins: [ ]
 };
