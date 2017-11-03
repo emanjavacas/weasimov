a68b83fa54208b82fb04081af4a97dee370d3e81
Folgert Karsdorp <fbkarsdorp@gmail.com>
2017-11-03 12:43:34 +0100

    server config (not for local usage)

---
+++ webservers/gunicorn.sh
@@ -0,0 +1,10 @@
+gunicorn -b 127.0.0.1:5000 -k eventlet -w 1 app.wsgi
+gunicorn -b 127.0.0.1:5001 -k eventlet -w 1 app.wsgi
+gunicorn -b 127.0.0.1:5002 -k eventlet -w 1 app.wsgi
+gunicorn -b 127.0.0.1:5003 -k eventlet -w 1 app.wsgi
+gunicorn -b 127.0.0.1:5004 -k eventlet -w 1 app.wsgi
+gunicorn -b 127.0.0.1:5005 -k eventlet -w 1 app.wsgi
+gunicorn -b 127.0.0.1:5006 -k eventlet -w 1 app.wsgi
+gunicorn -b 127.0.0.1:5007 -k eventlet -w 1 app.wsgi
+gunicorn -b 127.0.0.1:5008 -k eventlet -w 1 app.wsgi
+gunicorn -b 127.0.0.1:5009 -k eventlet -w 1 app.wsgi
